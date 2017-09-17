bab0b413ddd7efbb34e563b22e68321baffcf7ec
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-17 08:04:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const pretvornaTabela = [];
+pretvornaTabela[54] = 38;
+pretvornaTabela[53] = 37;
+pretvornaTabela[56] = 40;
 
 function init() {
   // Write your JavaScript code inside the init() function
