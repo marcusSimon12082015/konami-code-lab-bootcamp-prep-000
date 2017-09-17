462d46140e9324a8761c7a3a2117a1f073388852
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-17 08:14:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -24,7 +24,7 @@ function init() {
   }
   function convert(key){
     for (var k in pretvornaTabela) {
-      if (key === k) {
+      if (key === parseInt(k)) {
         key = pretvornaTabela[k];
       }
     }
