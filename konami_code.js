4435e4e4e85dff058e7d7a1bd71228c09277ea52
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-17 05:45:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const body = document.getElementsByTagName('body');
+  body.addEventListener('keydown', function(e){
+    
+  });
 }
